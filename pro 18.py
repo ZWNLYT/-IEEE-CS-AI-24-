d9bce7f916afd3bded0794bda11086740cc52d11
ZWNLYT <150627534+ZWNLYT@users.noreply.github.com>
2024-04-29 09:35:51 -0700

    Delete pro 18.py

--- pro 18.py
+++
@@ -1,9 +0,0 @@
-def xor_operation(a, b, q):
-    result = a
-    for _ in range(q - 1):
-        result = result ^ b
-    return result
-
-a, b, q = map(int, input().split())
-
-print(xor_operation(a, b, q))
