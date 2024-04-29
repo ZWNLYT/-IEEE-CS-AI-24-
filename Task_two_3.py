e2a03d19bfe7851659442017968798df8261a0a0
ZWNLYT <150627534+ZWNLYT@users.noreply.github.com>
2024-04-29 09:29:47 -0700

    Delete Task_two_3.py

--- Task_two_3.py
+++
@@ -1,6 +0,0 @@
-if __name__ == '__main__':
-    n = int(input())
-
-    arr = list(map(int, input().split()))
-    unique_sorted_arr = sorted(set(arr), reverse=True)
-    print(unique_sorted_arr[1])
